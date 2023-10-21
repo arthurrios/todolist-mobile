bbc384065cb744955df31e51909dc3708df602d1
arthur <arthur.rios007@gmail.com>
2023-10-20 21:16:14 -0300

    add input focus style

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,23 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { TextInput, TouchableOpacity, View } from 'react-native'
 import { Header } from '../components/Header'
 import { styles } from './styles'
 import { PlusCircle } from 'lucide-react-native'
+import { useState } from 'react'
 
 export function Home() {
+  const [isFocused, setIsFocused] = useState(false)
+
   return (
     <View style={styles.container}>
       <Header />
 
       <View style={styles.form}>
         <TextInput
-          style={styles.input}
+          style={isFocused ? styles.inputFocus : styles.input}
           placeholder="Adicione uma nova tarefa"
           placeholderTextColor="#808080"
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
         />
 
         <TouchableOpacity style={styles.button}>
